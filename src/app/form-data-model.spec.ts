4fb67287d315c78eea1e859f8edd636dc1601c96
Csengeri95 <csengerilevente95@gmail.com>
2023-08-20 20:17:06 +0200

    Auto-generated commit [ci skip]

--- src/app/form-data-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FormDataModel } from './form-data-model';
-
-describe('FormDataModel', () => {
-  it('should create an instance', () => {
-    expect(new FormDataModel()).toBeTruthy();
-  });
-});
